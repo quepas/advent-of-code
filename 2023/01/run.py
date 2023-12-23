86bb31707d71c4a81247b1c7fdab79ca70876afe
quepas <kiepas.patryk@gmail.com>
2023-12-23 18:46:26 +0100

    Add comments to the day 1

--- 2023/01/run.py
+++ 2023/01/run.py
@@ -1,4 +1,11 @@
-def parse_number(line: str, in_reverse: bool = False, with_words: bool = False) -> int:
+from typing import Optional
+
+
+def parse_number(line: str, in_reverse: bool = False, with_words: bool = False) -> Optional[int]:
+    """
+    Parse first encountered number (1-9) or its word representation (e.g. one, two, ...) in an input string.
+    This function can parse number starting from the beginning or the end.
+    """
     idx = len(line) - 1 if in_reverse else 0
     while len(line) > idx >= 0:
         if line[idx].isnumeric():
@@ -29,17 +36,24 @@ def parse_number(line: str, in_reverse: bool = False, with_words: bool = False) 
                 if line[idx:idx + 4] == "nine":
                     return 9
         idx += -1 if in_reverse else 1
-    # This should not happened
-    exit(-1)
+    return None
 
 
 def find_calibration_value(line: str, with_words: bool = False) -> int:
+    """
+    Find calibration value in an input string (also by parsing word representations of numbers).
+    """
+    # Find the first number
     first_num = parse_number(line, with_words=with_words)
+    # Find the last number
     last_num = parse_number(line, in_reverse=True, with_words=with_words)
+    # Compute a calibration number using the first and the last found numbers
     return 10 * first_num + last_num
 
 
 with open("input", "r") as f:
     lines = f.readlines()
-    print("Part one:", sum(map(find_calibration_value, lines)))
-    print("Part two:", sum(map(lambda line: find_calibration_value(line, with_words=True), lines)))
+    print("----- Part one -----")
+    print("Sum of calibration values:", sum(map(find_calibration_value, lines)))
+    print("----- Part two -----")
+    print("Sum of calibration values:", sum(map(lambda line: find_calibration_value(line, with_words=True), lines)))
