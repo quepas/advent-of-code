54fa43bf91661b26e76b9f3ecb7ed7440bfaaa13
quepas <kiepas.patryk@gmail.com>
2023-12-24 11:17:11 +0100

    Improve and add comments to day 2

--- 2023/02/run.py
+++ 2023/02/run.py
@@ -1,8 +1,14 @@
-def parse_game(line: str) -> dict:
+from operator import itemgetter
+
+
+def parse_single_game(line: str) -> dict:
+    """
+    Parse single game into a dictionary with a game id and maximal number of colored cubes in the game (RGB).
+    """
     chunks = line.split(":")
-    game = int(chunks[0].split(" ")[1])
+    game_id = int(chunks[0].split(" ")[1])
     d = {
-        "id": game,
+        "id": game_id,
         "red": 0,
         "green": 0,
         "blue": 0
@@ -10,23 +16,24 @@ def parse_game(line: str) -> dict:
     for s in chunks[1].split(";"):
         for cubes in s.split(","):
             num, color = cubes.strip().split(" ")
+            # Find maximal number of cubes of each color
             d[color] = max(d[color], int(num))
     return d
 
-max_red = 12
-max_green = 13
-max_blue = 14
 
-id_sum = 0
-id_prod_sum = 0
-with open("input", "r") as f:
-    for line in f.readlines():
-        game = parse_game(line)
-        if game["red"] <= max_red and game["green"] <= max_green and game["blue"] <= max_blue:
-            id_sum += game["id"]
-        game_prod = game['red']*game['blue']*game['green']
-        print(f"Game power {game['id']}: {game_prod}")
-        id_prod_sum += game_prod
-print(f"ID sum: {id_sum}")
-print(f"ID prod sum: {id_prod_sum}")
+def is_possible_game(game: dict, avail_red=12, avail_green=13, avail_blue=14) -> bool:
+    """
+    Check if a game is possible to play given the available number of cubes (RGB)
+    """
+    return game["red"] <= avail_red and game["green"] <= avail_green and game["blue"] <= avail_blue
 
+
+with open("input") as f:
+    all_games = list(map(parse_single_game, f.readlines()))
+    print("----- Part one -----")
+    possible_games = list(filter(is_possible_game, all_games))
+    sum_games_ids = sum(map(itemgetter("id"), possible_games))
+    print(f"Sum of possible games: {sum_games_ids}")
+    print("----- Part two -----")
+    sum_power_set_of_cubes = sum(map(lambda game: game["red"] * game["blue"] * game["green"], all_games))
+    print(f"Sum of the power of the minimum set of cubes: {sum_power_set_of_cubes}")
