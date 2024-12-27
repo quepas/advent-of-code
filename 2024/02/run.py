49ff9f6a1c199041c5064826d4b605da46d3475a
quepas <kiepas.patryk@gmail.com>
2024-12-27 01:15:36 +0100

    Finishing touch to day 2 python solution

--- 2024/02/run.py
+++ 2024/02/run.py
@@ -2,10 +2,14 @@ from common.numbers import difference
 from common.parsing import parse_line_of_ints
 
 
-def check_safe(elements: list[int]) -> bool:
-    num_diff = difference(elements)
-    return all(map(lambda e: 0 < e <= 3, num_diff)) or all(
-        map(lambda e: -3 <= e < 0, num_diff)
+def check_safe(levels: list[int]) -> bool:
+    """
+    Check if level differences are all increasing or all decreasing
+    and if they are in a specific range!
+    """
+    levels_diff = difference(levels)
+    return all(map(lambda e: 0 < e <= 3, levels_diff)) or all(
+        map(lambda e: -3 <= e < 0, levels_diff)
     )
 
 
@@ -13,19 +17,19 @@ with open("2024/02/input", "r") as f:
     lines = f.readlines()
     num_safe, num_fixed = 0, 0
     for line in lines:
-        row = parse_line_of_ints(line)
-        # Part 1
-        if check_safe(row):
+        levels = parse_line_of_ints(line)
+        # Part 1: count immediatly safe reports
+        if check_safe(levels):
             num_safe += 1
-        # Part 2
+        # Part 2: count reports which could be fixed
         else:
             # Find Problem Dampener
-            for i in range(len(row)):
+            for i in range(len(levels)):
                 # Holdout one number and then try testing !
-                new_row = row[:i] + row[i + 1 :]
+                new_row = levels[:i] + levels[i + 1 :]
                 if check_safe(new_row):
                     num_fixed += 1
                     break
 
-    print(f"Num safe (part 1)={num_safe}")
-    print(f"Num safe (part 2)={num_safe + num_fixed}")
+    print(f"Num. of safe reports (part 1)={num_safe}")
+    print(f"Num. of safe and fixed reports (part 2)={num_safe + num_fixed}")
