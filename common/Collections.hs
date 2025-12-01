1317aef5de1773656280e91701acab17e1e4e795
quepas <kiepas.patryk@gmail.com>
2025-12-01 09:57:56 +0100

    Add haskell useful functions

--- common/Collections.hs
+++ common/Collections.hs
@@ -5,9 +5,11 @@ module Collections
   )
 where
 
+pairwise :: (a -> a -> a) -> [a] -> [a]
 pairwise f xs = zipWith f xs (tail xs)
 
 pairwiseDifference :: (Num a) => [a] -> [a]
 pairwiseDifference = pairwise (-)
 
+allPairwise :: (a -> a -> Bool) -> [a] -> Bool
 allPairwise f xs = all (uncurry f) (zip xs (tail xs))
